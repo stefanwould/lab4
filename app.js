d73dfd554b35652e47d6689a2a38e6c01dd5e7df
Stefan Wooding <swooding@ucsd.edu>
2017-02-02 00:18:22 -0800

    node.js lab

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ var index = require('./routes/index');
 // Example route
 // var user = require('./routes/user');
 
+var index = require('./routes/index');
+var project = require('./routes/project');
+
+
 var app = express();
 
 // all environments
@@ -38,6 +42,8 @@ if ('development' == app.get('env')) {
 app.get('/', index.view);
 // Example route
 // app.get('/users', user.list);
+app.get('/project/:name', project.viewProject);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
